c4ee2c41d50d29deb63461bdc431f0cebbaa0dc9
kenji.amano <kenji.amano@rakuten.com>
2018-06-24 10:30:17 +0900

    add screens

--- main.ts
+++ main.ts
@@ -22,10 +22,10 @@ function createWindow() {
   if (serve) {
     require('electron-reload')(__dirname, {
      electron: require(`${__dirname}/node_modules/electron`)});
-    win.loadURL('http://localhost:4200');
+    win.loadURL('http://localhost:4200/#/message');
   } else {
     win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
+      pathname: path.join(__dirname, 'dist/index.html/#/message'),
       protocol: 'file:',
       slashes: true
     }));
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,49 @@
 import { HomeComponent } from './components/home/home.component';
+import { MessageComponent } from './components/message/message.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { ExplorerComponent } from './components/explorer/explorer.component';
+import { PeerComponent } from './components/peer/peer.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Profile } from 'selenium-webdriver/firefox';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    }
+    },
+    {
+        path: 'message',
+        component: MessageComponent
+    },
+    {
+        path: 'message-list',
+        component: MessageListComponent
+    },
+    {
+        path: 'user-profile',
+        component: UserProfileComponent
+    },
+    {
+        path: 'wallet',
+        component: WalletComponent
+    },
+    {
+        path: 'explorer',
+        component: ExplorerComponent
+    },
+    {
+        path: 'peer',
+        component: PeerComponent
+    },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
